cb7a5866faf35f0ceeb4e0bdf6ba07ece989bb8b
小丶k <kun771447@163.com>
2023-06-02 11:24:14 +0800

    feat: 物体添加渐变色

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export const color = {
+    // 建筑物颜色
+    mesh: '#1B3045',
+    // 建筑物顶部颜色
+    head: '#ffffff'
+}
--- src/effect/surroundLine.js
+++ src/effect/surroundLine.js
@@ -1,29 +1,65 @@
 import * as THREE from 'three';
+import { color } from '../config';
 
 export class SurroundLine {
     constructor(scene, child) {
         this.scene = scene;
         this.child = child;
         
+
+        // 模型颜色，底部显示颜色
+        this.meshColor = color.mesh;
+        // 头部颜色，顶部显示颜色
+        this.headColor = color.head;
+
         this.render();
     }
 
+    computedMesh() {
+        this.child.geometry.computeBoundingBox();
+        this.child.geometry.computeBoundingSphere();
+    }
+
     render() {
+        this.computedMesh();
+
+        const { max, min} = this.child.geometry.boundingBox;
+
+        // 高度差
+        const size = max.z - min.z;
+
         const material = new THREE.ShaderMaterial({ 
             uniforms: {
-                city_color: {
-                    value: new THREE.Color('#00008B')
+                u_city_color: {
+                    value: new THREE.Color(this.meshColor)
+                },
+                u_head_color: {
+                    value: new THREE.Color(this.headColor)
+                },
+                u_size: {
+                    value: size
                 }
             },
             vertexShader: `
+                varying vec3 v_position;
+
                 void main() {
+                    v_position = position;
+
                     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
                 }
             `,
             fragmentShader: `
-                uniform vec3 city_color;
+                varying vec3 v_position;
+                
+                uniform vec3 u_city_color;
+                uniform vec3 u_head_color;
+                uniform float u_size;
+                
                 void main() {
-                    gl_FragColor = vec4(city_color, 1.0);
+                    vec3 base_color = mix(u_city_color, u_head_color, v_position.z / u_size);
+
+                    gl_FragColor = vec4(base_color, 1.0);
                 }
             `
         });
